b6b4770fafaacfa1639b67f05e0c31e3f29fcf85
haidymahaer <haidymaherrr@gmail.com>
2023-12-23 18:28:34 +0200

    edit

--- server.js
+++ server.js
@@ -71,80 +71,106 @@ app.post('/upload', upload.single('file'), (req, res) => {
 });
 // Route to handle user login
 app.post('/login', async (req, res) => {
-    // Ensure req.body is not undefined and contains required fields
-    if (!req.body || !req.body.email || !req.body.password) {
-        return res.status(400).send('Invalid request body');
+  // Ensure req.body is not undefined and contains required fields
+  if (!req.body || !req.body.email || !req.body.password) {
+    return res.status(400).send('Invalid request body');
+  }
+
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.findOne({ email, password });
+
+    if (!user) {
+      return res.status(401).send('Invalid email or password');
     }
 
-    const { email, password } = req.body;
-
-    try {
-        const user = await User.findOne({ email, password });
-
-        if (!user) {
-            return res.status(401).send('Invalid email or password');
-        }
-
-        // You might want to implement a session or token mechanism for authentication
-        // For simplicity, just sending a success message for now
-        res.status(200).send('Login successful');
-    } catch (error) {
-        console.error('Error during login:', error);
-        res.status(500).send('Internal Server Error');
-    }
+    // You might want to implement a session or token mechanism for authentication
+    // For simplicity, just sending a success message for now
+    res.status(200).send('Login successful');
+  } catch (error) {
+    console.error('Error during login:', error);
+    res.status(500).send('Internal Server Error');
+  }
 });
 
 // Route to handle user signup
 app.post('/signup', async (req, res) => {
-    console.log('Request Body:', req.body);
-    // Ensure req.body is not undefined and contains required fields
-    if (!req.body || !req.body.username || !req.body.email || !req.body.password) {
-      return res.status(400).send('Invalid request body');
-      
+  console.log('Request Body:', req.body);
+  // Ensure req.body is not undefined and contains required fields
+  if (!req.body || !req.body.username || !req.body.email || !req.body.password) {
+    return res.status(400).send('Invalid request body');
+
+  }
+
+  const { username, email, password } = req.body;
+
+  try {
+    const existingUser = await User.findOne({ username });
+
+    if (existingUser) {
+      return res.status(409).send('User already exists');
     }
-  
-    const { username, email, password } = req.body;
-  
-    try {
-      const existingUser = await User.findOne({ username });
-  
-      if (existingUser) {
-        return res.status(409).send('User already exists');
-      }
-  
-      const newUser = new User({ username, email, password });
-      await newUser.save();
-  
-      res.status(201).json({ message: 'User created successfully' });
-    } catch (error) {
-      console.error('Error creating user:', error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
+
+    const newUser = new User({ username, email, password });
+    await newUser.save();
+
+    res.status(201).json({ message: 'User created successfully' });
+  } catch (error) {
+    console.error('Error creating user:', error);
+    res.status(500).send('Internal Server Error');
+  }
+});
 
 
-  
-  app.post('/createSession', async (req, res) => {
-    console.log('Request Body:', req.body);
-  
-    if (!req.body || !req.body.topicName || !req.body.date) {
-      return res.status(400).json({ error: 'Invalid request body' });
-    }
-  
-    const { topicName, date } = req.body;
-  
-    try {  
-      const newSession = new Sessions({ topicName, date });
-      await newSession.save();
-  
-      res.status(201).json({ message: 'Session created successfully' });
-    } catch (error) {
-      console.error('Error creating session:', error);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
 
-  // Route to get session details by ID
+app.post('/createSession', async (req, res) => {
+  console.log('Request Body:', req.body);
+
+  if (!req.body || !req.body.topicName || !req.body.date) {
+    return res.status(400).json({ error: 'Invalid request body' });
+  }
+
+  const { topicName, date } = req.body;
+
+  try {
+    const newSession = new Sessions({ topicName, date });
+    await newSession.save();
+
+    res.status(201).json({ message: 'Session created successfully' });
+  } catch (error) {
+    console.error('Error creating session:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+
+
+app.post('/updateSession/:id', async (req, res) => {
+  const sessionId = req.params.id;
+
+  const { topicName, date, attendanceNumber } = req.body;
+
+  try {
+    const session = await Sessions.findByIdAndUpdate(
+      sessionId,
+      { topicName, date, attendanceNumber },
+      { new: true } // Return the updated session
+    );
+
+    if (!session) {
+      return res.status(404).json({ error: 'Session not found' });
+    }
+
+    res.status(200).json({ message: 'Session updated successfully', session });
+  } catch (error) {
+    console.error('Error updating session:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+// Route to get session details by ID
 // Assuming this is your server setup and the Sessions model is defined properly
 
 app.get('/getSession/:id', async (req, res) => {
@@ -198,19 +224,19 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.get('/', (req, res) => {
-    res.render('home'); // Render the 'home.ejs' view
+  res.render('home'); // Render the 'home.ejs' view
 });
 
 app.get('/about', (req, res) => {
-    res.render('about'); // Render the 'about.ejs' view
+  res.render('about'); // Render the 'about.ejs' view
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact'); // Render the 'contact.ejs' view
+  res.render('contact'); // Render the 'contact.ejs' view
 });
 
 app.get('/login', (req, res) => {
-    res.render('login'); // Render the 'login.ejs' view
+  res.render('login'); // Render the 'login.ejs' view
 });
 
 
@@ -218,36 +244,36 @@ app.get('/login', (req, res) => {
 // FOR ALL PAGES IN NAV
 app.get('/sessions', async (req, res) => {
   try {
-      const sessions = await Sessions.find(); // Fetch sessions from the database
+    const sessions = await Sessions.find(); // Fetch sessions from the database
 
-      res.render('sessions', { sessions }); // Pass fetched sessions to the 'sessions.ejs' view
+    res.render('sessions', { sessions }); // Pass fetched sessions to the 'sessions.ejs' view
   } catch (error) {
-      console.error('Error fetching sessions:', error);
-      res.status(500).send('Internal Server Error');
+    console.error('Error fetching sessions:', error);
+    res.status(500).send('Internal Server Error');
   }
 });
 
 
 
 app.get('/createSession', (req, res) => {
-    res.render('createSession'); // Render the 'createSession.ejs' view
+  res.render('createSession'); // Render the 'createSession.ejs' view
 });
 
 app.get('/editSession', (req, res) => {
-    res.render('editSession'); // Render the 'editSession.ejs' view
+  res.render('editSession'); // Render the 'editSession.ejs' view
 });
 
 
 app.get('/students', (req, res) => {
-    res.render('sessions'); // Render the 'sessions.ejs' view
+  res.render('sessions'); // Render the 'sessions.ejs' view
 });
 
 app.get('/createStudent', (req, res) => {
-    res.render('createSession'); // Render the 'createSession.ejs' view
+  res.render('createSession'); // Render the 'createSession.ejs' view
 });
 
 app.get('/editStudent', (req, res) => {
-    res.render('editSession'); // Render the 'editSession.ejs' view
+  res.render('editSession'); // Render the 'editSession.ejs' view
 });
 
 // END
@@ -255,5 +281,5 @@ app.get('/editStudent', (req, res) => {
 
 // Start the server
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
